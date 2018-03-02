d9cd1a93174145079f873763ef7af327224537ca
Matt Sampson <matthew@therubythree.com>
2018-03-01 16:50:37 -0800

    README update and inital file commit

--- README.md
+++ README.md
@@ -1,25 +1,5 @@
-# README
-
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
 # TestApp
+
+This application's purpose is to act as a playground for my RoR knowledge. It will help me test the things that I learn as well as overcome problems that I run into.
+
+TestApp is built with Ruby on Rails
---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+6.times do |i|
+  Article.create!(
+    title: "Article #{i}",
+    body: "Etiam leo elit, dapibus a condimentum ut, malesuada rhoncus diam. Morbi hendrerit, tellus sit amet vestibulum vestibulum, erat felis aliquam sapien, non hendrerit risus elit nec mauris. Cras mattis lectus et turpis luctus posuere. Maecenas placerat consequat faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra rhoncus metus, ac dapibus purus pretium id. Phasellus tincidunt pulvinar auctor. Sed elementum augue vel felis cursus luctus."
+  )
+end
